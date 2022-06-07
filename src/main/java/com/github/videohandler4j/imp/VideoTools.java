6ca0ed0406c37a027a0b92a85e7be5bc3fd1967e
LEOOLIVE <LEOOLIVE@FDRD-18251.trf3.jus.br>
2022-06-07 17:56:26 -0400

    using environment variable instead of embedded binary

--- src/main/java/com/github/videohandler4j/imp/VideoTools.java
+++ src/main/java/com/github/videohandler4j/imp/VideoTools.java
@@ -27,12 +27,7 @@
 
 package com.github.videohandler4j.imp;
 
-import static com.github.utils4j.imp.Strings.trim;
-import static com.github.utils4j.imp.Throwables.tryRun;
 import static com.github.videohandler4j.imp.TimeTools.parse;
-import static java.lang.System.getProperty;
-import static java.nio.file.Files.copy;
-import static java.nio.file.Paths.get;
 
 import java.io.File;
 import java.io.InputStream;
@@ -62,22 +57,7 @@ public enum VideoTools implements Caller<File, IVideoFile, VideoDurationNotFound
   }
   
   public final Optional<Path> fullPath() {
-    return Optional.ofNullable(findPath());
-  }
-
-  private final synchronized Path findPath() {
-    return Environment.resolveTo("FFMPEG_HOME", fileName, true, true).orElseGet(() -> {
-      String home = trim(getProperty("user.home", ""));
-      if (home.isEmpty())
-        return null;
-      Path ffmpeg = get(home, fileName);
-      boolean success = tryRun(() -> {
-        try(InputStream input = VideoTools.class.getResourceAsStream("/vh4j/binaries/" + fileName)) {
-          copy(input, ffmpeg);
-        }
-      });
-      return success ? ffmpeg : null;
-    });
+    return Environment.resolveTo("FFMPEG_HOME", fileName, true, true);
   }
   
   @Override
