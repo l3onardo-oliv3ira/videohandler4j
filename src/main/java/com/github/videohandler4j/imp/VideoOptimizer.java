3785b50ce299df6d7cc50d99f481d4e76c9c0920
LEOOLIVE <LEOOLIVE@FDRD-18251.trf3.jus.br>
2022-07-11 14:26:00 -0400

    optimization profile (40 default)

--- src/main/java/com/github/videohandler4j/imp/VideoOptimizer.java
+++ src/main/java/com/github/videohandler4j/imp/VideoOptimizer.java
@@ -2,15 +2,24 @@ package com.github.videohandler4j.imp;
 
 import java.util.List;
 
+import com.github.utils4j.imp.Args;
+
 public class VideoOptimizer extends FFMPEGHandler {
 
+  private final int crf;
+  
   public VideoOptimizer() {
+    this(40);
+  }
+  
+  public VideoOptimizer(int crf) {
+    this.crf = Args.requirePositive(crf, "crf <= 0");
   }
   
   @Override
   protected final void fillParameters(List<String> commandLine) {
     commandLine.add("-crf");
-    commandLine.add("40");
+    commandLine.add(Integer.toString(crf));
     commandLine.add("-vf");
     commandLine.add("scale=trunc(iw/4)*2:trunc(ih/4)*2");
     commandLine.add("-max_muxing_queue_size");
